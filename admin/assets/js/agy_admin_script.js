46d0f2cd8c25cc94eceb1d812eb0000c4f2e1008
Marko <upss070288@gmail.com>
2021-01-14 14:52:18 +0100

    Final release 1.0.1 LTS

--- admin/assets/js/agy_admin_script.js
+++ admin/assets/js/agy_admin_script.js
@@ -1,9 +1,1 @@
-"use strict";
-const openTab = (e, t) => {
-    let a, l, s;
-    l = document.getElementsByClassName("agy-tabcontent");
-    for (let e = 0; e < l.length; e++) l[e].style.display = "none";
-    for (s = document.getElementsByClassName("agy-tablinks"), a = 0; a < s.length; a++) s[a].className = s[a].className.replace(" agy-active", "");
-    document.getElementById(t).style.display = "block", e.currentTarget.className += " agy-active"
-};
-document.getElementById("default-open").click();
+"use strict";const openTab=(e,t)=>{let a,l,s;l=document.getElementsByClassName("agy-tabcontent");for(let e=0;e<l.length;e++)l[e].style.display="none";for(s=document.getElementsByClassName("agy-tablinks"),a=0;a<s.length;a++)s[a].className=s[a].className.replace(" agy-active","");document.getElementById(t).style.display="block",e.currentTarget.className+=" agy-active"};document.getElementById("default-open").click();
