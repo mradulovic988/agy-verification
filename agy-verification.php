175193db3a6f748044618bda0c034b938fcf2d73
Marko Radulovic <markoradulovic@Markos-MacBook-Pro.local>
2021-02-25 23:14:45 +0100

    Changed version to 1.0.3

--- agy-verification.php
+++ agy-verification.php
@@ -11,7 +11,7 @@
  * Plugin Name:       Agy Verification
  * Plugin URI:        https://wordpress.org/plugins/agy-verification
  * Description:       Agy Verification is a powerful solution to add any kind of verification restriction on your website. Easy to setup, optimized for all devices, and modern design option to match your style.
- * Version:           1.0.1
+ * Version:           1.0.3
  * Requires at least: 4.6
  * Requires PHP:      7.1
  * Author:            Marko Radulovic
--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Requires at least: 4.6
 Requires PHP: 7.1
 Donate link: https://mlab-studio.com
 Tested up to: 5.6
-Stable tag: 1.0.1
+Stable tag: 1.0.3
 License: GPL v3 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
 
