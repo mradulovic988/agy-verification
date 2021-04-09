8765b617fb2e49cfb9aa02c35fc740af7b75a99d
Marko <upss070288@gmail.com>
2021-04-09 15:28:17 +0200

    Fixed bugs with plugin preloading

--- public/assets/js/agy_public_script.js
+++ public/assets/js/agy_public_script.js
@@ -1,4 +1,1 @@
-"use strict";
-document.querySelector("#agy-my-modal") && (document.querySelector("#agy-my-modal").style.display = "block"), document.querySelector(".agy-enter-btn button") && document.querySelector(".agy-enter-btn button").addEventListener("click", () => {
-    document.cookie = "agy_verification=approved", setTimeout(() => document.querySelector("#agy-my-modal").style.display = "none", 500)
-});
+"use strict";document.querySelector("#agy-my-modal")&&(document.querySelector("#agy-my-modal").style.display="block"),document.querySelector(".agy-enter-btn button")&&document.querySelector(".agy-enter-btn button").addEventListener("click",()=>{document.cookie="agy_verification=approved;path=/",setTimeout(()=>document.querySelector("#agy-my-modal").style.display="none",500)});
